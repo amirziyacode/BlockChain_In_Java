2de0c7c64cc3d9d8b5025d3e0f394a211a47c222
amirziyacode <amzdidi@gmail.com>
2025-12-09 12:35:21 +0330

    imp block

---
+++ src/main/java/org/example/model/Block.java
@@ -0,0 +1,148 @@
+package org.example.model;
+
+import java.io.Serializable;
+import java.security.InvalidKeyException;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import sun.security.provider.DSAPublicKeyImpl;
+
+public class Block  implements Serializable {
+
+
+    private byte[] prevHash;
+    private byte[] currentHash;
+    private byte[] minedBy;
+    private String timeStamp;
+    private Integer ledgerId = 1;
+    private Integer miningPoints = 0;
+    private Double luck = 0.0;
+
+    private ArrayList<Transaction> transactionLedger = new ArrayList<>();
+
+    public Block(byte[] prevHash,
+                 byte[] currentHash,
+                 byte[] minedBy, String timeStamp,
+                 Integer ledgerId,
+                 Integer miningPoints,
+                 Double luck, ArrayList<Transaction> transactionLedger) {
+        this.prevHash = prevHash;
+        this.currentHash = currentHash;
+        this.minedBy = minedBy;
+        this.timeStamp = timeStamp;
+        this.ledgerId = ledgerId;
+        this.miningPoints = miningPoints;
+        this.luck = luck;
+        this.transactionLedger = transactionLedger;
+    }
+
+    public byte[] getPrevHash() {
+        return prevHash;
+    }
+
+    public void setPrevHash(byte[] prevHash) {
+        this.prevHash = prevHash;
+    }
+
+    public byte[] getCurrentHash() {
+        return currentHash;
+    }
+
+    public void setCurrentHash(byte[] currentHash) {
+        this.currentHash = currentHash;
+    }
+
+    public byte[] getMinedBy() {
+        return minedBy;
+    }
+
+    public void setMinedBy(byte[] minedBy) {
+        this.minedBy = minedBy;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Integer getLedgerId() {
+        return ledgerId;
+    }
+
+    public void setLedgerId(Integer ledgerId) {
+        this.ledgerId = ledgerId;
+    }
+
+    public Integer getMiningPoints() {
+        return miningPoints;
+    }
+
+    public void setMiningPoints(Integer miningPoints) {
+        this.miningPoints = miningPoints;
+    }
+
+    public Double getLuck() {
+        return luck;
+    }
+
+    public void setLuck(Double luck) {
+        this.luck = luck;
+    }
+
+    public ArrayList<Transaction> getTransactionLedger() {
+        return transactionLedger;
+    }
+
+    public void setTransactionLedger(ArrayList<Transaction> transactionLedger) {
+        this.transactionLedger = transactionLedger;
+    }
+
+    public Block(LinkedList<Block> currentBlockChain) {
+        Block lastBlock = currentBlockChain.getLast();
+        prevHash = lastBlock.currentHash;
+        ledgerId = lastBlock.ledgerId + 1;
+        luck = Math.random() * 1000000;
+    }
+
+    public Block(){
+        prevHash = new  byte[]{0};
+    }
+
+    public Boolean isVerified(Signature sining) throws InvalidKeyException, SignatureException {
+        sining.initVerify(new DSAPublicKeyImpl(this.minedBy));
+        sining.update(this.toString().getBytes());
+        return sining.verify(this.currentHash);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Block)) return false;
+        Block block = (Block) o;
+        return Arrays.equals(getPrevHash(), block.getPrevHash());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getPrevHash());
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "prevHash=" + Arrays.toString(prevHash) +
+                ", timeStamp='" + timeStamp + '\'' +
+                ", minedBy=" + Arrays.toString(minedBy) +
+                ", ledgerId=" + ledgerId +
+                ", miningPoints=" + miningPoints +
+                ", luck=" + luck +
+                '}';
+    }
+}
